c7a46f5030f5312658f726006bd2325043e86b40
priom125 <priom6046@gmail.com>
2025-10-19 11:44:51 +0600

    bar chart done

--- src/components/AppDetailsCard.jsx
+++ src/components/AppDetailsCard.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import downloadsIMG from "../assets/icon-downloads.png";
 import avgRatings from "../assets/icon-ratings.png";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
 function AppDetailsCard({ app }) {
+  const ratings = app.ratings;
+  console.log(ratings);
   return (
     <div className="bg-[#D9D9D9]  p-20 text-black">
       <div className="flex gap-10">
@@ -35,13 +38,30 @@ function AppDetailsCard({ app }) {
               <p className="text-2xl font-bold">{app.reviews}</p>
             </div>
           </div>
-          <button className="btn mt-4 text-white bg-[#00D390] outline-0 border-0">Install Now {app.size} MB</button>
+          <button className="btn mt-4 text-white bg-[#00D390] outline-0 border-0">
+            Install Now {app.size} MB
+          </button>
         </div>
       </div>
       {/* Card ends here */}
 
       <div className="bar">
+        <BarChart
+          layout="vertical"
+          style={{
+            width: "100%",
+            maxWidth: "700px",
+            maxHeight: "70vh",
+          }}
+          data={ratings}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="count" type="number " />
 
+          <YAxis width="auto" dataKey="name" type="category" />
+
+          <Bar dataKey="count" fill="#FF8811" />
+        </BarChart>
       </div>
 
       {/* bar ends here */}
